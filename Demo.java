06e95dfd8f79e0915e5d669f0b8440f449e2ebe9
Saikumar Reddy <saikumar_reddy@thbs.india.com>
2023-12-14 10:23:20 +0530

    Java Demo

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+   public static void main(String args[])
+   {
+       System.out.println("Hello.....");
+       int a=10;
+       int b=20;
+       System.out.println("Addition of two numbers: "+(a+b));
+   }
+}
